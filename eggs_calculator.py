cf32bf5adc082b3722659a08855bbfb8d3d24573
Menaka Siriwardhana <99058518+DevMenaka@users.noreply.github.com>
2025-10-17 00:16:28 +0530

    eggs_calculator

---
+++ eggs_calculator.py
@@ -0,0 +1,7 @@
+get_eggs_number = int(input("Enter Number Of Eggs ?"))
+
+egg_holders_of_box = 12
+
+print(f"How Many Boxes Of Eggs : {get_eggs_number // egg_holders_of_box}")
+
+print(f"Remainng Eggs : {get_eggs_number % egg_holders_of_box }")
